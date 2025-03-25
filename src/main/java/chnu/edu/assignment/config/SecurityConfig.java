f7427579520223806d2f34ae50a3fe3c9e24f448
Dmytro Sydoruk <dima.sudoryk@gmail.com>
2025-03-25 13:27:25 +0200

    UPDATE SecurityConfig and app properties: add users with different roles CREATE static files for different roles

--- src/main/java/chnu/edu/assignment/config/SecurityConfig.java
+++ src/main/java/chnu/edu/assignment/config/SecurityConfig.java
@@ -7,12 +7,17 @@ package chnu.edu.assignment.config;
     @version 1.0.0
     @since  24.03.2025 - 21.05       
 */
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,10 +25,48 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-            .httpBasic(Customizer.withDefaults());
-        return http.build();  
+                .authorizeHttpRequests(req ->
+                        req
+                                .requestMatchers("/index.html").permitAll()
+                                .requestMatchers("/user.html").hasAnyRole("USER", "ADMIN", "SUPERADMIN")
+                                .requestMatchers("/admin.html").hasAnyRole("ADMIN", "SUPERADMIN")
+                                .requestMatchers("/superuser.html").hasRole("SUPERADMIN")
+                                .requestMatchers("/admin_only.html").hasRole("ADMIN")
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
+
+        UserDetails superadmin = User.builder()
+                .username("superadmin")
+                .password(passwordEncoder().encode("superadmin"))
+                .roles("SUPERADMIN")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(admin, user, superadmin);
     }
 }
